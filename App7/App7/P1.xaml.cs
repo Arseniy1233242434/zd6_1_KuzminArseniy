1bb085d31a02f9d6a740282df2e24e15eecf0416
user <senyakuzmin29@gmail.com>
2024-12-05 11:10:16 +0500

    first commit

---
+++ App7/App7/P1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class P1 : ContentPage
+    {
+        public P1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
